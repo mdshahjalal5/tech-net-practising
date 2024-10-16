ca19494a1212e35e283b5e7f1106fdf687ecffc9
Md. Shahjalal Sheikh Bin Karsk <muhommodshahjalal@gmail.com>
2024-10-16 16:39:09 +0600

    done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <h2>
+      <p className="bg-blue-900   text-blue-300">
+        ami shahjalal tmi ke go bou naki amar
+      </p>
+    </h2>
+  );
+}
+
+export default App;
